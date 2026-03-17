b6c835dfcd9067e195aef452aa63f64d9fac33c0
nils <pluetzer@interaktiv.de>
2026-03-17 12:17:49 +0100

    Add pnpm-lock.yaml file to track dependencies and ensure reproducible builds

--- frontend/packages/volto-clean-volto-template/src/index.ts
+++ frontend/packages/volto-clean-volto-template/src/index.ts
@@ -4,6 +4,10 @@ import installSettings from './config/settings';
 function applyConfig(config: ConfigType) {
   installSettings(config);
 
+  config.settings.isMultilingual = true;
+  config.settings.supportedLanguages = ['en', 'de'];
+  config.settings.defaultLanguage = 'de';
+  
   return config;
 }
 
